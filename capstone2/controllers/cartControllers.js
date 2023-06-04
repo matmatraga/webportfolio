ac2a512de09a830b34af1d584cee9601e430a6ea
matthewmondoy.raga <matthewmondoy.raga@gmail.com>
2023-06-04 11:59:24 +0800

    add partial codes for capstone2

--- capstone2/controllers/cartControllers.js
+++ capstone2/controllers/cartControllers.js
@@ -16,21 +16,36 @@ module.exports.addedProducts = (request, response) => {
 // Change product quantities
 module.exports.changeProductQuantities = (request, response) => {
 
-	const products = request.params.id;
+	const cart = request.params.id;
 	const updatedProductQuantities = {
 		userId : request.body.userId,
 		products : request.body.products
 	}
 	
 
-	Cart.findByIdAndUpdate(products, updatedProductQuantities, {new:true})
+	Cart.findByIdAndUpdate(cart, updatedProductQuantities, {new:true})
 	.then(result => {
 		if(!result){
-			return response.send("Product ID is not found!")
+			return response.send("Cart ID is not found!")
 		}else {
-			return response.send(result)
+			return response.send("Product quantity has been changed!")
 		}
 	}).catch(error => response.send(error))
 }
 
 // Remove Products from Cart
+module.exports.removeProducts = (request, response) => {
+
+	const cart = request.params.id;
+	
+	Cart.findByIdAndDelete(cart, {userId : request.body.userId}, {new:true})
+	.then(result => {
+		if(!result){
+			return response.send("Cart ID is not found!")
+		}else {
+			return response.send("Product has been removed!")
+		}
+	}).catch(error => response.send(error))
+}
+
+// Subtotal for each item
--- capstone2/routes/cartRoutes.js
+++ capstone2/routes/cartRoutes.js
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.post("/cart", cartControllers.addedProducts);
 
-router.put("/cart/products:_id", cartControllers.changeProductQuantities);
+router.put("/cart/:id", cartControllers.changeProductQuantities);
+
+router.delete("/cart/:id", cartControllers.removeProducts);
+
 module.exports = router;
